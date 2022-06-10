3dbfba4abd746923edf13fe5dbea4ebb066e677f
Utkarshini12 <arorautkarshini@gmail.com>
2022-06-10 06:11:14 +0530

    movie details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,25 @@
-
-
-import React from "react";
-const Footer = () => {
-
-return (
-    <div>
-    <footer className="page-footer font-small white pt-4">
-                        <div className="footer-copyright text-center py-3">© 2022 Copyright : 
-                            <a href="https://relevel.com">&nbsp;Relevel by Unacademy</a>
+function Footer() {
+    return (
+        <div className="bg-dark text-light" >
+            <div className="bg-black py-2">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-xs-12">
+                            <i class="bi bi-film text-danger px-2 "></i> <span className="fw-bolder">List your Show</span>
                         </div>
-                    </footer>
-                    
-    </div>
-)
+                        <div className="col-lg-6 col-xs-12 text-center">
+                            Got a show, event, activity or a great experience? Partner with us & get listed on MBA! </div>
+                    </div>
+                </div>
+            </div>
+            <div className="my-auto p-2">
+            <h4 className="text-center text-danger my-auto fw-bolder">
+                MBA
+            </h4>
+            </div>
+            
+        </div>
+    )
 }
 
 export default Footer;
-
-
-
--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams,Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './MovieDetail.css';
-import {getMovie} from '../../api/movie'
+import { getMovie } from '../../api/movie'
 import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
+import './MovieDetail.css'
 
 const MovieDetail = () => {
     const { movieid: id } = useParams();
@@ -12,7 +14,7 @@ const MovieDetail = () => {
     const [MovieDetails, setMovieDetails] = useState({});
     const [releaseStatus, setMovieReleaseStatus] = useState(false);
     const [movieCast, setMovieCast] = useState([]);
-    
+
 
     const init = async () => {
 
@@ -24,46 +26,77 @@ const MovieDetail = () => {
         setMovieDetails(response.data);
         setMovieReleaseStatus(response.data.releaseStatus === "RELEASED")
         setMovieCast(response.data.casts)
-    
-      }
 
-    
+    }
+
+
     useEffect(() => {
         init();
-      },
+    },
         //eslint-disable-next-line
-    []);
+        []);
 
 
-return (
-    <div >
-        <h3>{MovieDetails.name}</h3>
-        <div className="row">
-            <div className="col">
-                <img src={MovieDetails.posterUrl} className="card" width={250} height={350}  alt="..."/>
-            </div>
-            <div className="col">
-                <div  className="card" >
-                    <ReactPlayer url={MovieDetails.trailerUrl} controls={true}/>
+    return (
+        <>
+            <Navbar />
+            <div className="bg-light">
+
+                <div className="box bg-black">
+                    <ReactPlayer url={MovieDetails.trailerUrl} controls={true} className="video" width="67%"
+                        height="99%" />
                 </div>
+
+                <div className="container my-4">
+                    <div className="row">
+                        <div className="col">
+                            <img src={MovieDetails.posterUrl} className="card" width={300} height={450} alt="..." />
+                        </div>
+                        <div className="col ">
+                            <h2 className="fw-bolder">About The Movie</h2>
+
+                            <div className="d-flex">
+                            <span class="badge rounded-pill text-bg-danger m-1"> {MovieDetails.description}</span>
+                            <span class="dot my-auto"></span>
+                                <span class="badge rounded-pill text-bg-secondary m-1">{MovieDetails.language}</span>
+                                <span class="dot my-auto"></span>
+                                <span class="badge rounded-pill text-bg-secondary m-1">{MovieDetails.releaseStatus}</span>
+                              
+                              
+                                
+                            </div>
+
+                            <hr />
+                            <h3>{MovieDetails.name}</h3>
+                            <h6>{MovieDetails.director}</h6>
+                            <h6>{MovieDetails.releaseDate}</h6>
+
+                            <hr />
+                            <h5>Cast</h5>
+                            {movieCast.map(name => <li className="list-group-item">{name}</li>)}
+
+                            <div className="text-center my-3"><Link key={selectedMovie} className="text-decoration-none btn btn-lg btn-danger text-center" to={releaseStatus ? `/buytickets/${MovieDetails.name}/${selectedMovie}` : `#`}>{releaseStatus ? "BOOK TICKET" : "COMMING SOON"}</Link></div>
+
+
+
+                            <br />
+
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
             </div>
-        </div>
-        <div>
-            
-            CAST: {movieCast.map(name => <li>{name}</li>)}
-            DESCRIPTION: <li>{MovieDetails.description}</li>
-            <li>{MovieDetails.director}</li>
-            <li>{MovieDetails.language}</li>
-            <li>{MovieDetails.releaseDate}</li>
-            <li>{MovieDetails.releaseStatus}</li>
-        </div>
-        
-        <Link key={selectedMovie} className="movie-items" to={releaseStatus ? `/buytickets/${MovieDetails.name}/${selectedMovie}` : `#`}>{releaseStatus ? "BOOK TICKET":"COMMING SOON"}</Link>
-        
-       <br />
-       <Footer/>
-    </div>
-)
+        </>
+    )
 }
 
 
